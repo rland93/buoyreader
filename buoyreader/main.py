17a6865e7b90f1b58ee5ad8130263e5fcc9df284
Mike Sutherland <ms@Michaels-MacBook-Pro.local>
2017-12-31 00:19:01 -0800

    added pandas dataframe export (more naive)

--- buoyreader/main.py
+++ buoyreader/main.py
@@ -1,29 +1,33 @@
 from urllib import request
 import datetime
 import numpy as np
+import pandas as pd
 
 # West Point Loma Buoy 46232
 
 def get_buoy_data(buoy_number, buoy_root="http://www.ndbc.noaa.gov/data/realtime2/"):
     """NOAA Buoy Number and URL -> Data in ascii
     Keyword Arguments:
-    buoy_number -- the NOAA buoy number
-    buoy_root -- base url for NOAA buoys. Defaults to http://www.ndbc.noaa.gov/data/realtime2/
+    ``buoy_number``: the NOAA buoy number
+    ``buoy_root``: base url for NOAA buoys. Defaults to http://www.ndbc.noaa.gov/data/realtime2/
     """
     buoy_url = buoy_root + "{}.txt".format(str(buoy_number))
     bdata = request.urlopen(buoy_url, data=None)
     # urlobject -> str
     with bdata as response:
-        raw_data = response.read().decode("ascii") 
+        raw_data = response.read().decode("ascii")
     return raw_data
 
 def read_historical_data(textfile):
     """read a textfile instead of live data.
     
+    Arguments:
+    ``textfile``: some plain text file with buoy data.
+
     Whole lot of those can be found here:
     http://www.ndbc.noaa.gov/historical_data.shtml
     """
-    with open(textfile, mode = "r") as file:
+    with open(textfile, "r") as file:
         raw_data = file.read()
     return raw_data
     
@@ -33,7 +37,7 @@ def raw_data_to_array(raw_data):
     data = []
     for line in raw_data.splitlines():
         if not line.startswith("#"):
-            data.append(line_format(line.split()))    
+            data.append(line_format(line.split()))
     return data
             
 def swell_quality(waveheight,waveperiod):
@@ -92,9 +96,10 @@ def delineate_equinox(date_ymd):
     """
     # shitty workaround... idk how to compare dates naive of the year
     if datetime.date(2000,3,20) > date_ymd.date().replace(year=2000) >= datetime.date(2000,9,23):
-        return "Summer"
+        equinox = "Summer"
     else:
-        return "Winter"
+        equinox = "Winter"
+    return equinox
 
 def line_format(split_line):
     """Prune unimportant info from a line and return dict with pruned values."""
@@ -137,8 +142,57 @@ def ldatetime(line):
     * ``line``: Single line ['yyyy','mm','dd','hh','mm']
     """
     return datetime.datetime(
-                            int(line[0]),
-                            int(line[1]),
-                            int(line[2]),
-                            int(line[3]),
-                            int(line[4]))
+        int(line[0]),
+        int(line[1]),
+        int(line[2]),
+        int(line[3]),
+        int(line[4]))
+
+def format_data(buoy_data):
+    """Bouy data as string -> Pandas dataframe
+
+    Args:
+
+    ``buoy_data``: buoy data (string)
+    """
+    # first line will have heading info.
+    heading = []
+    subheading = []
+    buoy_data = buoy_data.splitlines()
+
+    # Build headings:
+    for i, line in enumerate(buoy_data):
+        if i == 0 and line.startswith("#"):
+            heading = line.split()
+        if i == 1 and line.startswith("#"):
+            subheading = line.split()
+    for index, (value_h, value_s) in enumerate(zip(heading, subheading)):
+        # Remove "#" from headings.
+        if value_h.startswith("#"):
+            value_h = value_h.replace("#", "")
+        if value_s.startswith("#"):
+            value_s = value_s.replace("#", "")
+        # Combine headings
+        heading[index] = (value_h + " (" + value_s + ")")
+    # Build data:
+    data = []
+    # datetimes will become the index of our dataframe.
+    datetimes = []
+    # cases of "missing data" we want to replace
+    missing_data_to_replace = frozenset(["999", "99.0", "9999.0", "999.0", "999.0", "99.00"])
+    for i, line in enumerate(buoy_data):
+        if i > 2:
+            # build lines
+            split_line = line.split()
+            # some of the NOAA data has "99" or "99.0" or "999.0"
+            # in cases where data is missing; we don't want numerical
+            # values so we replace it.
+            split_line = [np.NaN if x in missing_data_to_replace else x for x in split_line]
+            # for n, item in enumerate(split_line):
+            #     if item in missing_data_to_replace:
+            #         split_line[n] = np.NaN
+            data.append(split_line)
+            # build index
+            datetimes.append(ldatetime(split_line))
+    return pd.DataFrame(data, columns=[i for i in heading], index=datetimes)
+    
